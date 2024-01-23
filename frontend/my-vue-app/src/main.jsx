2abcd3aab954145ce37efb17c310988bbd3ae097
Ricardo Merlin <rcrdmerlin@gmail.com>
2024-01-23 10:20:24 -0500

    uploading changes

--- frontend/my-vue-app/src/main.jsx
+++ frontend/my-vue-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CreateProfile from './CreateProfile.jsx'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
