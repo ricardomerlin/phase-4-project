82813b8aac7240395997cd3192df58b10b311edb
Ricardo Merlin <rcrdmerlin@gmail.com>
2024-01-24 00:23:32 -0500

    uploading

--- frontend/my-vue-app/src/Login.jsx
+++ frontend/my-vue-app/src/Login.jsx
@@ -1,39 +1,34 @@
-import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
-function Login({}) {
+function Login() {
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const navigate = useNavigate();
 
-    // function loginInfoTransport() {
-    //     bringUsernameUp(username)
-    //     bringPasswordUp(password)
-    // }
-
+    const setSignup = () => {
+        // Navigate to createProfilePage
+        navigate('/createprofile');
+    }
 
     return(
         <div>
-            <h1>Login with Spotify</h1>
-            <h2>Email Address</h2>
-            <form >
-                <input
-                type='text'
-                name='username'
-                className='username-input'
-                onChange={(e) => setUsername(e.target.value)}
-                />
-                <h3>Password</h3>
-                <input
-                type='text'
-                name='password'
-                // placeholder='Enter desired password'
-                className='password-input'
-                onChange={(e) => setPassword(e.target.value)}
-                />
-                <div>
-                    <button className = 'signup-button'>New user? Create a new account here.</button>
-                </div>
-            </form>
+            <h2>Username</h2>
+            <input
+            type='text'
+            name='username'
+            className='username-input'
+            onChange={(e) => setUsername(e.target.value)}
+            />
+            <h3>Password</h3>
+            <input
+            type='text'
+            name='password'
+            // placeholder='Enter desired password'
+            className='password-input'
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <div>
+                <button className = 'signup-button' onClick = {setSignup}>New user? Create a new account here.</button>
+            </div>
         </div>
     )
 
